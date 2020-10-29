1d80d43ebbe85e13511974583b7adb850db62d5e
Thomas Heuveline <thomasheuveline61@gmail.com>
2020-10-29 18:59:58 +0100

    create form task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task", name="task")
+     * @Route("/tasks/listing", name="task_listing")
      */
-    public function index(): Response
+    public function taskLinsting(): Response
     {
+        //On va chercher par doctrine le repository de nos tasks
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        //Dans ce repository nous récupérons toutes les données
+        $tasks = $repository->findAll();
+
+        //affichage des données dans le var_dumper
+        // dd($tasks);
+
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,36 +1,43 @@
 <?php
-
 namespace App\DataFixtures;
-
-use Faker\Factory;
+use App\Entity\Tag;
 use App\Entity\Task;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // Create a new Faker object
+        // Création d'un nouvel objet Faker
         $faker = Factory::create('fr_FR');
-
-        // Crate between 15 and 30 task object
-        for($i = 0; $i < mt_rand(15, 30); $i++){
-
-            //Crate a new task object
+        // création de nos 5 catégories
+        for ($c = 0; $c < 5; $c++) {
+            // création d'un nouvel objet Tag
+            $tag = new Tag;
+            // On ajoute un nom à notre catégorie
+            $tag->setName($faker->colorName());
+            // On fait persister les données
+            $manager->persist($tag);
+        }
+        // On push les catégories en BDD
+        $manager->flush();
+        // récupération des catégories créées
+        $allTags = $manager->getRepository(Tag::class)->findAll();
+        // Création entre 15 et 30 tâches aléatoirement
+        for ($t = 0; $t < mt_rand(15, 30); $t++) {
+            // Création d'un nouvel objet Task
             $task = new Task;
-            
-            //Feed task object
+            // On nourrit l'objet Task
             $task->setName($faker->sentence(6))
                 ->setDescription($faker->paragraph(3))
-                ->setCreatedAt(new \DateTime())
-                ->setDueAt($faker->dateTimeBetween('now', '6 month'));
-
-                //Persist datas
-                $manager->persist($task);
+                ->setCreatedAt(new \Datetime())
+                ->setDueAt($faker->dateTimeBetween('now', '6 months'))
+                ->setTag($faker->randomElement($allTags));
+            // On fait persister les données
+            $manager->persist($task);
         }
-
-        //Push
+        // On push le tout en BDD
         $manager->flush();
     }
 }
